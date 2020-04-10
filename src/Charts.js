57e46264c7ebdd67385041fccba5d09a7e724c36
kennja05 <jacobkenny05@gmail.com>
2020-04-10 17:24:23 -0400

    initial setup

---
+++ src/Charts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Charts extends React.Component {
+    
+    render(){
+        return(
+            <div>Chart Goes here</div>
+        )
+    }
+}
