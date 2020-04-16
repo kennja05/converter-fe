08f38d9fe4f46309b7769f2ebc1ac28c56c75d22
kennja05 <jacobkenny05@gmail.com>
2020-04-16 18:21:14 -0400

    updated fetching of historical rates so that only one call is made to the BE

--- src/HistoricalInfo.js
+++ src/HistoricalInfo.js
@@ -4,32 +4,11 @@ export default class HistoricalInfo extends React.Component {
     
     state = {
         base: 'USD',
-        oldRates: []
+        historicalRates: []
     }
 
     componentDidMount(){
         this.getRates()
-    }
-
-    getHistoricalRates = () => {
-        const base = this.state.base
-        const target = this.props.match.params.code
-        console.log(target)
-        const historicalRateArray = [];
-        const myDate = new Date();
-        const formattedCurrentDate = myDate.toISOString().split('T')[0] //gets todays date in yyy-mm-dd format
-        for (let i = 1; i < 6; i++) {
-            const dateArr = formattedCurrentDate.split('-')
-            dateArr[0] = parseInt(dateArr[0])- i
-            const prevDate = dateArr.join('-')
-            console.log(prevDate)
-            fetch(`http://localhost:3000/countries/historical/rates/${base}/${target}/${prevDate}/`)
-                .then(resp => resp.json())
-                .then(rate => historicalRateArray.push(rate))
-        }
-        this.setState({
-            oldRates: historicalRateArray
-        })
     }
 
     getRates = () => {
@@ -40,11 +19,12 @@ export default class HistoricalInfo extends React.Component {
         console.log(formattedCurrentDate)
         fetch(`http://localhost:3000/countries/historical/rates/${base}/${target}/${formattedCurrentDate}/`)
             .then(resp => resp.json())
-            .then(info => console.log(info))
+            .then(pastRates => this.setState({
+                historicalRates: pastRates
+            })
     }
 
     render(){
-        console.log(this.state.oldRates)
         return(
             <h2>
                 This same conversion if it were done 1 year ago
