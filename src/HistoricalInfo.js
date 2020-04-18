2a7bf454c6206db1868d288de7c4c412972c65b5
kennja05 <jacobkenny05@gmail.com>
2020-04-17 23:07:29 -0400

    wrote function to create coordinates for line graph

--- src/HistoricalInfo.js
+++ src/HistoricalInfo.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import CanvasJSReact from './canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 
 export default class HistoricalInfo extends React.Component {
     
     state = {
         base: 'USD',
-        historicalRates: []
+        historicalRates: [],
+        coordinates: []
     }
 
     componentDidMount(){
@@ -18,20 +22,53 @@ export default class HistoricalInfo extends React.Component {
         const formattedCurrentDate = myDate.toISOString().split('T')[0]
         fetch(`http://localhost:3000/countries/historical/rates/${base}/${target}/${formattedCurrentDate}/`)
             .then(resp => resp.json())
-            .then(pastRates => this.setState({
-                historicalRates: pastRates
-            }))
-        }
+            .then(pastRates => this.createCoordinates(pastRates))
+    }
+
+    createCoordinates = ratesArray => {
+        console.log(ratesArray)
+        const myArr = [];
+        for (let i = 0; i < ratesArray.lenth; i++){
+            let myObj = {x: ratesArray[i].date}
+            myArr.push(myObj)
+            console.log(myObj)
+        } 
+        this.setState({
+            coordinates: myArr
+        })
+        console.log(myArr)
+    }
 
     render(){
+        const options = {
+            title:{text: `${this.state.base} to ${this.props.match.params.code}`},
+            exportEnabled: true,
+            data: [{
+                type: "line",
+                dataPoints: [
+                { x: 1, y: 450 },
+                { x: 2, y: 414 },
+                { x: 3, y: 520 },
+                { x: 4, y: 460 },
+                { x: 5, y: 450 },
+                { x: 6, y: 500 },
+                { x: 7, y: 480 },
+                { x: 8, y: 480 },
+                { x: 9, y: 410 },
+                { x: 10, y: 500 },
+                { x: 11, y: 480 },
+                { x: 12, y: 510 }
+              ]}]
+          }
         return(
             <div>
                 <h2>
-                Historical Rates 
+                Historical Rates: {this.state.base} to {this.props.match.params.code}
                 </h2>
                 <ul>
                     {this.state.historicalRates.map((rate, index) => <li key={index}>{rate.date} rate: {Object.values(rate.rates)}</li>)}
                 </ul>
+                <CanvasJSChart options={options}/>
             </div>
         )
     }
