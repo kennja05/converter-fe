81f01431671269f5ac6e26e608f78df347dda2e6
kennja05 <jacobkenny05@gmail.com>
2020-04-23 09:10:03 -0400

    updated formatting for all countries list

--- src/AllCountries.js
+++ src/AllCountries.js
@@ -40,10 +40,10 @@ export default class AllCountries extends React.Component {
                     {this.state.countries.slice(this.state.startIndex, this.state.startIndex + 50).map(country => 
                     <li className='list-group-item' key={country.id}>
                         <span>{country.name}</span> -  
-                        <span> {country.currency_name} 
+                        <span> {country.currency_name} - 
                         <span className='country-link' onClick={this.handleCodeClick} id={country.currency_code}>({country.currency_code})</span>
-                        </span>
-                        <Link to={{pathname: '/convert', state:{convertFrom: country}}}>Perform a Conversion</Link>
+                        </span> - 
+                        <Link to={{pathname: '/convert', state:{convertFrom: country}}}> Perform a Conversion</Link>
                     </li>)}
                 </ul>
                 <button onClick={this.handlePrevClick} style={{color: '#e98074'}}className='btn btn-light'>Previous</button>  <button onClick={this.handleNextClick} style={{color: '#e98074'}} className='btn btn-light'>Next</button>
